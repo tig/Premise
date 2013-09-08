e79e751712347414f6025f1a617db931df3fd807
Charlie Kindel <charlie@kindel.com>
2013-09-08 09:16:56 -0700

    broken win8

---
+++ PremiseLib/PremiseMetro/PremiseMetro/PremiseCommands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PremiseLib {
+    public class PremiseCommand : ICommand {
+        private string _propertyName;
+        private dynamic _holdingObject;
+
+        public PremiseCommand(PremiseObject holdingObject, string propertyName) {
+            _holdingObject = holdingObject;
+            _propertyName = propertyName;
+        }
+
+        public string PropertyName {
+            get { return _propertyName; }
+            set {
+                _propertyName = value;
+                if (_holdingObject != null && !String.IsNullOrEmpty(_propertyName)) {
+                    OnCanExecuteChanged();
+                }
+            }
+        }
+
+        public PremiseObject HoldingObject {
+            get { return _holdingObject; }
+            set {
+                _holdingObject = value;
+                if (_holdingObject != null && !String.IsNullOrEmpty(_propertyName)) {
+                    OnCanExecuteChanged();
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter) {
+            return _holdingObject != null && !String.IsNullOrEmpty(_propertyName);
+        }
+
+        public void Execute(object parameter) {
+            if (_holdingObject != null && !String.IsNullOrEmpty(_propertyName))
+                _holdingObject.SetMember(_propertyName, true);
+        }
+
+        public event EventHandler CanExecuteChanged;
+        protected virtual void OnCanExecuteChanged() {
+            EventHandler handler = CanExecuteChanged;
+            if (handler != null) handler(this, new EventArgs());
+        }
+    }
+}
